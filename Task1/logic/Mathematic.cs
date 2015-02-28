a53f521ed7f5de64a88d1c80f4d72b24433ce575
dubodel-alexey <dubodel.alexey@gmail.com>
2015-03-01 00:36:50 +0300

    tasks 1, 2

---
+++ Task1/logic/Mathematic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public static class Mathematic
+    {
+        public static double GetRoot(double number, int power)
+        {
+            if (power == 0) return 1;
+            if (Math.Abs(number) < 0.0001) return 0;
+
+            double eps = 1e-20;
+            double previousResult, newResult = 1;
+            do
+            {
+                previousResult = newResult;
+                newResult = (1.0 / power) * ((power - 1) * previousResult + number / Math.Pow(previousResult, power - 1));
+            } while (Math.Abs(previousResult - newResult) >= eps);
+
+            return newResult;
+        }
+    }
+}
---
+++ Task2/logic/SortingClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace logic
+{
+    public static class SortingClass
+    {
+        public static void SortByElementsSum(double[][] targerArray, bool direct)
+        {
+            var array = new double[targerArray.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = targerArray[i].Sum();
+            }
+
+            Sort(targerArray, array, direct);
+        }
+
+        public static void SortByElementsSum(int[][] targerArray, bool direct)
+        {
+            var array = new int[targerArray.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = targerArray[i].Sum();
+            }
+
+            Sort(targerArray, array, direct);
+        }
+
+        public static void SortByMaxElements<T>(T[][] targerArray, bool direct) where T : IComparable<T>
+        {
+            var array = new T[targerArray.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = targerArray[i].Max();
+            }
+
+            Sort(targerArray, array, direct);
+        }
+
+        public static void SortByMinElements<T>(T[][] targerArray, bool direct) where T : IComparable<T>
+        {
+            var array = new T[targerArray.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = targerArray[i].Min();
+            }
+
+            Sort(targerArray, array, direct);
+        }
+
+        private static void Sort<T>(T[][] targerArray, T[] array, bool direct) where T : IComparable<T> 
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (direct)
+                    {
+                        if (array[j].CompareTo(array[i]) < 0) 
+                        {
+                            Swap(ref array[j],ref array[i]);
+                            Swap(ref targerArray[j],ref targerArray[i]);
+                        }
+                    }
+                    else
+                    {
+                        if (array[j].CompareTo(array[i]) > 0) 
+                        {
+                            Swap(ref array[j], ref array[i]);
+                            Swap(ref targerArray[j], ref targerArray[i]);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Swap<T>(ref T firstItem, ref T secondItem)
+        {
+            T temp = firstItem;
+            firstItem = secondItem;
+            secondItem = temp;
+        }
+    }
+}
